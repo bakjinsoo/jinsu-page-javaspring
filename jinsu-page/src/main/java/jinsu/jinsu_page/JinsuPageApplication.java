afb4fa7c44fa0cb0851019d849c3b81e5f811a63
PARK JIN SOO <77185565+bakjinsoo@users.noreply.github.com>
2024-06-24 11:05:04 +0900

    Add files via upload

---
+++ jinsu-page/src/main/java/jinsu/jinsu_page/JinsuPageApplication.java
@@ -0,0 +1,13 @@
+package jinsu.jinsu_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinsuPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JinsuPageApplication.class, args);
+	}
+
+}
---
+++ jinsu-page/src/test/java/jinsu/jinsu_page/JinsuPageApplicationTests.java
@@ -0,0 +1,13 @@
+package jinsu.jinsu_page;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JinsuPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
